3961b097db646c758654422d9075968a288f0d2f
IlliaStrelsov <55940607+IlliaStrelsov@users.noreply.github.com>
2020-11-30 13:57:20 +0200

    Add files via upload

--- OOP python.py
+++ OOP python.py
@@ -3,7 +3,17 @@ import math
 import pymongo
 
 
-class DataBase:
+class SingletonMetaclass(type):
+    __instance = {}
+
+    def __call__(cls,*args,**kwargs):
+        if cls not in cls.__instance:
+            cls.__instance[cls] = super().__call__(*args,**kwargs)
+        return cls.__instance[cls]
+
+
+class DataBase(metaclass=SingletonMetaclass):
+
     def insert(self,continent:str,country:str,city:str,longitude:float,latitude:float):
         myclient = pymongo.MongoClient("mongodb://localhost:27017/")
         mydb = myclient["mydatabase"]
@@ -17,8 +27,7 @@ class DataBase:
         mycol = mydb["Studing"]
         myquery = {"City":city}
         mydoc = mycol.find(myquery)
-        for x in mydoc:
-            return x
+        return mydoc[0]
 
 
 
@@ -220,166 +229,170 @@ class WrongSpeedNumber(Exception):
 
 def run():
     while True:
-        print("1.Distance beetwen 2 cities")
-        print("2.Find distance between 2 points")
-        print("3.Get information about city")
-        print("4.Exit")
-        x = int(input())
-        if x == 1:
-            print("Enter a name of the first city")
-            first = str(input())
-            print("Enter a name of the second city")
-            second = str(input())
-            find = DataBase()
-            try:
-                coordinates = find.select(first)
-                cor1 = coordinates["longitude_coordinate"]
-                cor2 = coordinates["latitude_coordinate"]
-                first_city = Cordinates(cor1,cor2)
-                coordinate = find.select(second)
-                cor11 = coordinate["longitude_coordinate"]
-                cor21 = coordinate["latitude_coordinate"]
-                second_city = Cordinates(cor11,cor21)
-            except TypeError:
-                print("Sorry but we don`t know such city ")
-                print("1.Add this city to DataBase")
-                print("2.Exit to main menu")
-                y = int(input())
-                if y == 1:
-                    try:
-                        print("Enter a continent where city is located:")
-                        cont = str(input())
-                        print("Enter a country where city is located:")
-                        coun = str(input())
-                        print("Enter a name of city:")
-                        city = str(input())
-                        print("Enter a longitude coordinate:")
-                        longitude = float(input())
-                        print("Enter a latitude coordinate")
-                        latitude = float(input())
-                        cord = Cordinates(longitude,latitude)
-                        find.insert(cont,coun,city,cord.y,cord.x)
-                    except WrongCoordinates as e:
-                        print(f'{e}')
-                    finally:
-                        continue
-                if y ==2:
-                    continue
-            d = DirectDistance()
-            distance = d.get_distance(first_city,second_city)
-            l = DistantType()
-            distance = l.change_into_km_from_meters(distance)
-            print("Distance is:",int(distance),"km")
-            print("1.Get time of travel")
-            print("2.Exit")
-            p = int(input())
-            if p == 1:
-                print("Choose transport:")
-                print("1.Car")
-                print("2.Train")
-                print("3.Aircraft")
-                o = int(input())
-                if o == 1:
-                    car = Car()
-                    print("1.Set speed")
-                    print("2.Set default speed(50 km per hour)")
-                    m = int(input())
-                    if m == 1:
+        try:
+            print("1.Distance beetwen 2 cities")
+            print("2.Find distance between 2 points")
+            print("3.Get information about city")
+            print("4.Exit")
+            x = int(input())
+            if x == 1:
+                print("Enter a name of the first city")
+                first = str(input())
+                print("Enter a name of the second city")
+                second = str(input())
+                find = DataBase()
+                try:
+                    coordinates = find.select(first)
+                    cor1 = coordinates["longitude_coordinate"]
+                    cor2 = coordinates["latitude_coordinate"]
+                    first_city = Cordinates(cor1,cor2)
+                    coordinate = find.select(second)
+                    cor11 = coordinate["longitude_coordinate"]
+                    cor21 = coordinate["latitude_coordinate"]
+                    second_city = Cordinates(cor11,cor21)
+                except TypeError:
+                    print("Sorry but we don`t know such city ")
+                    print("1.Add this city to DataBase")
+                    print("2.Exit to main menu")
+                    y = int(input())
+                    if y == 1:
                         try:
-                            print("Enter a value of speed(km per hour):")
-                            j = int(input())
-                            car.set_speed(j)
+                            print("Enter a continent where city is located:")
+                            cont = str(input())
+                            print("Enter a country where city is located:")
+                            coun = str(input())
+                            print("Enter a name of city:")
+                            city = str(input())
+                            print("Enter a longitude coordinate:")
+                            longitude = float(input())
+                            print("Enter a latitude coordinate")
+                            latitude = float(input())
+                            cord = Cordinates(longitude,latitude)
+                            find.insert(cont,coun,city,cord.y,cord.x)
+                        except WrongCoordinates as e:
+                            print(f'{e}')
+                        finally:
+                            continue
+                    if y ==2:
+                        continue
+                d = DirectDistance()
+                distance = d.get_distance(first_city,second_city)
+                l = DistantType()
+                distance = l.change_into_km_from_meters(distance)
+                print("Distance is:",int(distance),"km")
+                print("1.Get time of travel")
+                print("2.Exit")
+                p = int(input())
+                if p == 1:
+                    print("Choose transport:")
+                    print("1.Car")
+                    print("2.Train")
+                    print("3.Aircraft")
+                    o = int(input())
+                    if o == 1:
+                        car = Car()
+                        print("1.Set speed")
+                        print("2.Set default speed(50 km per hour)")
+                        m = int(input())
+                        if m == 1:
+                            try:
+                                print("Enter a value of speed(km per hour):")
+                                j = int(input())
+                                car.set_speed(j)
+                                time = TravelTime()
+                                timing = time.get_time(distance,car.speed)
+                                print("Time of travel is about",int(timing),"hours\n\n\n")
+                            except WrongSpeedNumber as e:
+                                print(f'{e}\n\n\n')
+                            except WrongInput as e:
+                                print(f'{e}\n\n\n')
+                            except ValueError:
+                                print(f'Wrong input\n\n\n')
+                        if m ==2:
                             time = TravelTime()
-                            timing = time.get_time(distance,car.speed)
-                            print("Time of travel is about",int(timing),"hours\n\n\n")
-                        except WrongSpeedNumber as e:
-                            print(f'{e}\n\n\n')
-                        except WrongInput as e:
-                            print(f'{e}\n\n\n')
-                        except ValueError:
-                            print(f'Wrong input\n\n\n')
-                    if m ==2:
+                            timing = time.get_time(distance, car.speed)
+                            print("Time of travel is about", int(timing), "hours\n\n\n")
+
+                    if o == 2:
+                        train = Train()
                         time = TravelTime()
-                        timing = time.get_time(distance, car.speed)
+                        timing = time.get_time(distance, train.speed)
                         print("Time of travel is about", int(timing), "hours\n\n\n")
 
-                if o == 2:
-                    train = Train()
-                    time = TravelTime()
-                    timing = time.get_time(distance, train.speed)
-                    print("Time of travel is about", int(timing), "hours\n\n\n")
+                    if o == 3:
+                        aircraft = Aircraft()
+                        time = TravelTime()
+                        timing = time.get_time(distance, aircraft.speed)
+                        print("Time of travel is about", int(timing), "hours\n\n\n")
 
-                if o == 3:
-                    aircraft = Aircraft()
-                    time = TravelTime()
-                    timing = time.get_time(distance, aircraft.speed)
-                    print("Time of travel is about", int(timing), "hours\n\n\n")
-
-            if p == 2:
-                continue
-
-        if x == 4:
-            break
-
-        if x == 3:
-            find = DataBase()
-            print("Enter name of city:")
-            city = str(input())
-            try:
-                info  = find.select(city)
-                continent = info["Continent"]
-                country = info["Country"]
-                city = info["City"]
-                longitude = info["longitude_coordinate"]
-                latitude = info["latitude_coordinate"]
-                print(city,"is located on",continent,"in country",country,"with such coordinates of longitube:",longitude," and latitude:",latitude ,'\n\n\n')
-            except TypeError:
-                print("Sorry but we don`t know such city ")
-                print("1.Add this city to DataBase")
-                print("2.Exit to main menu")
-                y = int(input())
-                if y == 1:
-                    try:
-                        print("Enter a continent where city is located:")
-                        cont = str(input())
-                        print("Enter a country where city is located:")
-                        coun = str(input())
-                        print("Enter a name of city:")
-                        city = str(input())
-                        print("Enter a longitude coordinate:")
-                        longitude = float(input())
-                        print("Enter a latitude coordinate")
-                        latitude = float(input())
-                        cord = Cordinates(longitude, latitude)
-                        find.insert(cont, coun, city, cord.y, cord.x)
-                    except WrongCoordinates as e:
-                        print(f'{e}')
-                    finally:
-                        continue
-                if y ==2:
+                if p == 2:
                     continue
-        if x == 2:
-            print("Enter a longitude of the first point")
-            long1= float(input())
-            print("Enter a latitudeof the first point")
-            lati1 = float(input())
-            print("Enter a longitude of the second point")
-            long2 = float(input())
-            print("Enter a latitudeof the second point")
-            lati2 = float(input())
-            try:
-                a = Cordinates(long1,lati1)
-                b = Cordinates(long2,lati2)
-                d = DirectDistance()
-                c = d.get_distance(a,b)
-                l = DistantType()
-                print("\n\n",int(l.change_into_km_from_meters(c)),"km\n\n\n")
-            except NameError as e:
-                print(f'Error:{e}\n\n\n')
-            except WrongInput as e:
-                print(f'{e}\n\n\n')
-            except WrongCoordinates as e:
-                print(f'{e}\n\n\n')
+
+            if x == 4:
+                break
+
+            if x == 3:
+                find = DataBase()
+                print("Enter name of city:")
+                city = str(input())
+                try:
+                    info  = find.select(city)
+                    continent = info["Continent"]
+                    country = info["Country"]
+                    city = info["City"]
+                    longitude = info["longitude_coordinate"]
+                    latitude = info["latitude_coordinate"]
+                    print(city,"is located on",continent,"in country",country,"with such coordinates of longitube:",longitude," and latitude:",latitude ,'\n\n\n')
+                except TypeError:
+                    print("Sorry but we don`t know such city ")
+                    print("1.Add this city to DataBase")
+                    print("2.Exit to main menu")
+                    y = int(input())
+                    if y == 1:
+                        try:
+                            print("Enter a continent where city is located:")
+                            cont = str(input())
+                            print("Enter a country where city is located:")
+                            coun = str(input())
+                            print("Enter a name of city:")
+                            city = str(input())
+                            print("Enter a longitude coordinate:")
+                            longitude = float(input())
+                            print("Enter a latitude coordinate")
+                            latitude = float(input())
+                            cord = Cordinates(longitude, latitude)
+                            find.insert(cont, coun, city, cord.y, cord.x)
+                        except WrongCoordinates as e:
+                            print(f'{e}')
+                        finally:
+                            continue
+                    if y ==2:
+                        continue
+            if x == 2:
+                print("Enter a longitude of the first point")
+                long1= float(input())
+                print("Enter a latitudeof the first point")
+                lati1 = float(input())
+                print("Enter a longitude of the second point")
+                long2 = float(input())
+                print("Enter a latitudeof the second point")
+                lati2 = float(input())
+                try:
+                    a = Cordinates(long1,lati1)
+                    b = Cordinates(long2,lati2)
+                    d = DirectDistance()
+                    c = d.get_distance(a,b)
+                    l = DistantType()
+                    print("\n\n",int(l.change_into_km_from_meters(c)),"km\n\n\n")
+                except NameError as e:
+                    print(f'Error:{e}\n\n\n')
+                except WrongInput as e:
+                    print(f'{e}\n\n\n')
+                except WrongCoordinates as e:
+                    print(f'{e}\n\n\n')
+        except ValueError:
+            print("Wrong Input")
+            continue
 
 
 run()
